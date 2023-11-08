51db6254b97379ca3a5fedb80237181dee0afc1b
Ahmad Software <dr.ahmad.salah.54@gmail.com>
2023-11-09 00:53:07 +0300

    fix typescript

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
-import { QueryProvider } from "./lib/react-query/queryProvider";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
 
 const domNode: any = document.getElementById("root");
 const root = createRoot(domNode);
